c104711b877526c3da912f2ce5dbf5706bfcadec
Daniele Guido <gui.daniele@gmail.com>
2015-03-10 09:47:43 +0100

    example file for settings.js

---
+++ settings.example.js
@@ -0,0 +1,12 @@
+/**
+
+  Config file
+  ===
+*/
+module.exports = {
+  ALCHEMYAPI_KEY: 'your alchemy api key', // cfr alchemyapi.com
+  REKOGNITIONAPI_KEY : 'your rekognition qapi key', // cfr rekognition.com
+  REKOGNITIONAPI_SECRET : 'your rekognition api secret',
+  SKYBIOMETRYAPI_KEY : 'your skybiometry api', // cfr skybiometry https://www.skybiometry.com/Account
+  SKYBIOMETRYAPI_SECRET : 'your skybiometry api secret' 
+};
