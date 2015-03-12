2b90642ad662a3e52d5c756ba34d26cdc8fa0f35
Daniele Guido <gui.daniele@gmail.com>
2015-03-12 15:13:46 +0100

    adding express client

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-
+## installation
 npm install
 
 
@@ -17,3 +17,17 @@ test-single.jpg
  
 	
 	npm run-script migrate
+
+
+## typekit
+´´´javascript
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'fwe7tir',
+      scriptTimeout: 3000
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+´´´
--- server.js
+++ server.js
@@ -4,50 +4,67 @@
 // =============================================================================
 
 // call the packages we need
-var express     = require('express'),        // call express
-    settings    = require('./settings'),
-    app         = exports.app = express(),                 // define our app using express
-    bodyParser  = require('body-parser'),
-    ctrl        = require('require-all')(__dirname + '/controllers'),
+var express      = require('express'),        // call express
+    settings     = require('./settings'),
+    app          = exports.app = express(),                 // define our app using express
+    bodyParser   = require('body-parser'),
+    ctrl         = require('require-all')(__dirname + '/controllers'),
 
-    port = process.env.PORT || 8080,
+    port         = process.env.PORT || 8080,
 
-    router = express.Router();        // set our port
+    clientRouter = express.Router(),
+    apiRouter    = express.Router();        // set our port
+
+
+// configure static files and jade templates
+app.use(express.static('./client/src'));
+app.set('views', './client/views');
+app.set('view engine', 'jade');
+
 
 // configure app to use bodyParser(), this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/*
+  Client router configuration
+*/
+clientRouter.route('/').
+  get(function(req, res) { // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+    res.render('index', { message: 'hooray! welcome to our api!' });   
+  });
 
-
-router.use(function(req, res, next) { // middleware to use for all requests
+/*
+  Api router configuration
+*/
+apiRouter.use(function(req, res, next) { // middleware to use for all requests
     // do logging, redirect in case of unauth request @todo
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // api index
-router.route('/').
+apiRouter.route('/').
   get(function(req, res) { // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
     res.json({ message: 'hooray! welcome to our api!' });   
   });
 
 // face recognition tests
-router.route('/alchemyapi/image-face-tags')
+apiRouter.route('/alchemyapi/image-face-tags')
   .post(ctrl.alchemyapi.imageFaceTags.url)
 
-router.route('/rekognition/face-detect')
+apiRouter.route('/rekognition/face-detect')
   .post(ctrl.rekognition.faceDetect)
 
-router.route('/rekognition/face-search')
+apiRouter.route('/rekognition/face-search')
   .post(ctrl.rekognition.faceSearch)
 
-router.route('/skybiometry/face-detect')
+apiRouter.route('/skybiometry/face-detect')
   .post(ctrl.skybiometry.faceDetect)
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-// app.use('/', router); // client
-app.use('/api', router);
+app.use('/', clientRouter); // client
+app.use('/api', apiRouter);
 
 // START THE SERVER
 // =============================================================================
