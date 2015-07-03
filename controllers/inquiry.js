97b9534edba0cd4afdc292201ffeedbddd3d5ea3
Daniele Guido <gui.daniele@gmail.com>
2015-07-03 12:09:54 +0200

    get single inquiry

--- controllers/inquiry.js
+++ controllers/inquiry.js
@@ -19,26 +19,32 @@ module.exports = function(io) {
       get a single inquiry, the resource attached.
     */
     getItem: function (req, res) {
-    
+      var form = validator.request(req);
+      if(!form.isValid)
+        return helpers.formError(err, res);
+      inquiry.get(+form.params.id, function (err, item) {
+        return helpers.models.getOne(err, res, item, form.params);
+      });
     },
     
     /*
       get some inquiries
     */
     getItems: function (req, res) {
-      
-      validator.queryParams(req.query, function (err, params, warnings) {
-        if(err)
-          return helpers.formError(err, res);
+      var form = validator.request(req, {
+        limit: 10,
+        offset: 0
+      });
+      if(!form.isValid)
+        return helpers.formError(err, res);
         
-        inquiry.getMany(params, function (err, items) {
-          return helpers.models.getMany(err, res, items, params, warnings);
-        });
+      inquiry.getMany(form.params, function (err, items) {
+        return helpers.models.getMany(err, res, items, form.params);
       });
     },
     
     /*
-      create an inquiry
+      create an inquiry. Cfr resource create related inquiry instead.
     */
     create: function(req, res) {
       // validate params here
