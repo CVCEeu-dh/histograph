efd11d5eaf44d802356549a5b8781ea8ac93e2f4
Daniele Guido <gui.daniele@gmail.com>
2015-10-09 12:46:34 +0200

    Added utils section with mergeMany function
    MergeMany function merge every nodes existing relationships (merge
    candidate clusters are grouped by a property specified by the user)

--- scripts/tasks/helpers.js
+++ scripts/tasks/helpers.js
@@ -1,16 +1,192 @@
 /*
   Shared tasks for manage.js script
-  npm manage --task=
+  
 */
-var settings  = require('../../settings'),
-
+var settings   = require('../../settings'),
+    neo4j      = require('seraph')(settings.neo4j.host),
     fs         = require('fs'),
     csv        = require('csv'),
-    generator  = require('../../generator')();
+    generator  = require('../../generator')(),
+    inquirer   = require('inquirer'),
     exectimer  = require('exectimer');
     
 module.exports = {
-  
+  utils: {
+    
+    /*
+      merge by options.property two or more nodes from the options.records collection. .
+      Always ask for confirmation before merging.
+    */
+    mergeMany: function(options, callback) {
+      console.log(clc.yellowBright('\n   tasks.helpers.utils.mergeMany'));
+      if(_.isEmpty(options.property)) {
+        return callback(' Please specify the property with --property');
+      }
+      console.log(clc.blackBright('   merge by:'), options.property);
+      var groups,
+          toBeUpdated,
+          toBeMerged;
+      // groups of rows by propoerty ;)    
+      groups = _.values(_.groupBy(options.records, options.property));
+      
+      // nodes only to be updated
+      toBeUpdated = _.flatten(groups.filter(function (d) {
+        return d.length == 1
+      }));
+      
+      // nodes to be merged
+      toBeMerged = groups.filter(function (d) {
+        return d.length > 1
+      });
+      
+      // remove groups from memory (we creaed a copy)
+      delete groups
+      
+      // output to the user
+      console.log(clc.blackBright('   to be updated:', clc.magentaBright(toBeUpdated.length), '- to be merged:',clc.magentaBright(_.sum(toBeMerged, function (d) {return d.length})), 'nodes, in', clc.magentaBright(toBeMerged.length)));
+    
+      // first things first: update what need to be updated, with user confirmation
+      var checkNodes = [];
+      async.series([
+        
+        function merge(next) {
+          var q = async.queue(function (group, nextGroup) {
+            var ids = _.map(group, function (d) {return +d.id}),
+                names = _.map(group, 'name');
+            
+            console.log(clc.blackBright('\n   merging nodes:',  clc.cyanBright(JSON.stringify(ids)), 'remaining:'),q.length());
+            // collect all the relationships for the nodes
+            neo4j.query('MATCH (n) WHERE id(n) in {ids} WITH n MATCH p=(n)-[r]-(t) RETURN n as node, r as rel', {
+              ids: ids
+            }, function (err, tuples) {
+              if(err) {
+                q.kill();
+                next(err);
+                return;
+              };
+              // ged differences in properties
+              
+              var ghosts = tuples.map(function (tuple) {
+                var rel = tuple.rel
+                if(ids.indexOf(rel.start) != -1 && rel.start != ids[0]) {
+                  console.log(rel.start, '===== s ', ids[0]);
+                  rel.new_start  = ids[0];
+                  rel.new_end = rel.end
+                  rel.CHANGE = true; 
+                } else if(ids.indexOf(rel.end) != -1 && rel.end != ids[0]) {
+                  console.log(rel.end, '===== e ', ids[0]);
+                  rel.new_start  =  rel.start;
+                  rel.new_end = ids[0]
+                  rel.CHANGE = true; 
+                }
+                return rel;
+              });
+              
+              var clones = _.values(_.groupBy(ghosts, function(d) {
+                // everything except the ID
+                return [d.start, d.end, d.type, JSON.stringify(d.properties)].join();
+              }));
+              
+              var relToBeRemoved = _.flatten(clones.filter(function (d) {
+                return d.length > 1;
+              }).map(function (d) {
+                return _.takeRight(_.map(d, 'id'), d.length -1);
+              }));
+              
+              var relToBeUpdated = _.flatten(clones.map(function(d) {
+                return _.first(d, 1);
+              })).filter(function (d) {
+                return d.CHANGE
+              });
+              
+              var nodes = _.indexBy(_.map(tuples, 'node'), 'id');
+              
+             
+              
+              async.series([
+                function compareNodeContents(nextStep) {
+                  var needToBeReviewedBeforeMerging = false;
+                  console.log(clc.blackBright('   slug:'), clc.cyanBright(_.first(_.map(group, 'slug'))));
+                  console.log(clc.blackBright('   name:'),names);
+                  
+                  var quietlyUpdatable = {};
+                  
+                  for(var i = 0; i < ids.length; i++) {
+                    for(var j = i; j < ids.length; j++) {
+                      //var fields = _difference(_.keys(nodes[ids[i]], nodes[ids[j]]))
+                      for(var k in nodes[ids[i]])
+                        if(['id', 'full_search', 'caption_search', 'textrazor_annotated'].indexOf(k) == -1
+                            && nodes[ids[i]][k] != nodes[ids[j]][k]
+                              
+                        ) {
+                          if(_.isEmpty(nodes[ids[i]][k]) || _.isEmpty(nodes[ids[j]][k])) {
+                            quietlyUpdatable[k] = _.unique(_.compact((quietlyUpdatable[k] || []).concat(nodes[ids[i]][k], nodes[ids[j]][k])))
+                            continue;
+                          }
+                          if(typeof nodes[ids[i]][k] != 'object' || nodes[ids[i]][k].join() != nodes[ids[j]][k].join() )
+                            k != 'full_search' && console.log(clc.yellowBright('   ',k),': \n    "', nodes[ids[i]][k] ,'" != \n    "', nodes[ids[j]][k], '"')  
+                            needToBeReviewedBeforeMerging = true;
+                        }
+                    }
+                  }
+                  if(!needToBeReviewedBeforeMerging) {
+                    console.log(clc.yellowBright('   nodes can be merged quietly'), 'resulting in:');
+                    
+                    nextStep();
+                    return
+                  }
+                  
+                  inquirer.prompt([{
+                    type: 'confirm',
+                    name: 'confirm',
+                    message: ' The two nodes look very, very different ... continue the merging process?',
+                  }], function (answers) {
+                    if(answers.confirm) {
+                      nextStep();
+                    } else {
+                      console.log(clc.yellowBright('skipping merge ...'));
+                      nextStep('bad')
+                    }
+                  })
+                },
+                
+                function merge(nextStep) {
+                  console.log(clc.blackBright('   remove relationships:'), relToBeRemoved)
+                  console.log(clc.blackBright('   update relationships:'), relToBeUpdated)
+                  if(relToBeUpdated.length + relToBeRemoved.length == 0) {
+                    console.log(clc.blackBright('   nothing to do, skipping', clc.cyanBright(JSON.stringify(ids))));
+                    nextGroup();
+                    return;
+                  };
+                  inquirer.prompt([{
+                    type: 'confirm',
+                    name: 'confirm',
+                    message: ' Press enter to MERGE or REMOVE the selected relationships, otherwise SKIP by typing "n"',
+                  }], function (answers) {
+                    if(answers.confirm) {
+                      nextGroup();
+                      return;
+                    } else {
+                      console.log(clc.blackBright('   skipped, nothing changed for', clc.cyanBright(JSON.stringify(ids))));
+                      nextGroup();
+                      return;
+                    }
+                  });
+                }
+              ], nextGroup);
+            });
+          },1);
+          q.push(toBeMerged);
+          q.drain = next;
+        },  
+      ], function (err, results) { //eof async.series
+        if(err)
+          callback(err)
+        else
+          callback(null, options)
+      });     
+    }
+  },
   tick: {
     start: function(options, callback) {
       options.__tick = new exectimer.Tick("TIMER");
