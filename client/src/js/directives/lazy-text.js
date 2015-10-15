f89840d6577238b1bc7edef57fa53d1be51a458f
Daniele Guido <gui.daniele@gmail.com>
2015-10-15 12:39:21 +0200

    adding infiniteScrill directive

--- client/src/js/directives/lazy-text.js
+++ client/src/js/directives/lazy-text.js
@@ -29,4 +29,66 @@ angular.module('histograph')
         })
       }
     }
-  });
+  })
+  /*
+    Basic infinite-scroll directive for ui-views
+  */
+  .directive('infiniteScroll', function($log) {
+    'use strict';
+    
+    return {
+      scope: {
+        infiniteScroll: '&',
+        infiniteScrollCollection: '='
+      },
+      link: function(scope, elem, attrs) {
+        $log.log('::infinite-scroll ready');
+        
+        var scrollable = angular.element(elem), // wrap elem with jquery.
+            distance   = 0.7, // the distance ratio for scrollableHeight
+            throttle   = 660, // wait for animtion to set a proper height
+            scrollableHeight,
+            timer;
+        
+        // scope vars
+        scope.isBusy = false;
+        
+        // scope fn
+        scope.scrolling = function(e){
+          if(scope.busy)
+            return;
+          
+          var h = scrollable[0].scrollHeight,
+              t = scrollable.scrollTop(),// local scrolltop. we will then add the scrollable height to get the lowest visible point.
+              l = h - (scrollableHeight * distance); // the limit
+          
+          if(t + scrollableHeight > l) {
+            $log.log('::infinite-scroll @scrolling', 'launch scroll');
+            scope.isBusy = true;
+            scope.infiniteScroll();
+          } 
+        }
+        
+        
+        scope.$watch('infiniteScrollCollection', function(items) {
+          if(!items.length) {
+            scope.isBusy = false;
+            return;
+          }
+          
+          $log.log('::infinite-scroll @infiniteScrollCollection, n.items:', items.length);
+          scrollableHeight = scrollable.height();
+          if(timer)
+            clearTimeout(timer);
+          timer= setTimeout(function() {
+            scope.isBusy = false;
+            scope.$apply();
+          }, throttle);
+          
+        });
+        
+        // start listening on scrolling events.
+        scrollable.on('scroll', scope.scrolling);
+      }
+    }
+  })
